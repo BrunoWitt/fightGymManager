075e02319cec6d362a223b91c3b4ad6e28dda37e
Bruno Witt <brunowittn@gmail.com>
2026-01-13 16:31:44 -0300

    configurações inicias do backend

--- frontEnd/src/pages/loginPage/Login.jsx
+++ frontEnd/src/pages/loginPage/Login.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+
 export default function Home() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log("Email:", email)
+        console.log("Password:", password)
+        //Adicionar função para requistar o login para o backend
+    }
+
     return (
-        <div className="p-8">
-        <h1 className="text-3xl font-bold">Página de login</h1>
-        </div>
+        <form onSubmit={handleLogin}>
+            <div>
+                Login Page
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email"></input>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password"></input>
+                <button type="submit">Logar</button>
+            </div>
+        </form>
     )
 }
