1cc73a433b00f3dc2c92295334c2a77f89d8171c
Bruno Witt <brunowittn@gmail.com>
2026-01-14 15:46:26 -0300

    feat: começo da produção da home page

--- frontEnd/src/routes/AppRoutes.jsx
+++ frontEnd/src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/loginPage/Login'
+import Home from '../pages/homePage/Home'
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
         </Routes>
     )
 }
