22c12a10b49f7b085b267e5bcd73cf27a305bba9
Bruno Witt <brunowittn@gmail.com>
2026-01-15 08:36:16 -0300

    feat: planejamento no front sobre o calendario e configs inicias para iniciar o back de turmas

--- frontEnd/src/pages/homePage/components/Calender.jsx
+++ frontEnd/src/pages/homePage/components/Calender.jsx
@@ -1,7 +1,9 @@
 export default function Calender() {
     return (
         <div>
-
+            /*Calendario é dependente de turma
+            será responsável por mostrar todos os horarios de turma e irá requisitar para o back /calender/horarios(ou algo assim)
+            poderá editar também e mercar coisas no calendário */
         </div>
     );
 }
